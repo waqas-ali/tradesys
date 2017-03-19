faaa57e20cb2c2922654646402e3f36879d28444
waqas-ali <waqas.gemini186@yahoo.com>
2017-03-19 15:18:32 +0500

    Database Integration Service Project added

---
+++ DatabaseIntegerationService/src/main/java/com/trade/database/App.java
@@ -0,0 +1,13 @@
+package com.trade.database;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
